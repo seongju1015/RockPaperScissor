7b41d6293e18cb510c1ab4a5b26223093d770bb5
sj_mon <122343583+seongju1015@users.noreply.github.com>
2023-08-11 17:48:43 +0900

    Update Game.java
    add game

--- Game.java
+++ Game.java
@@ -1,3 +1,29 @@
+package project.game;
+import java.util.Random;
+
 public class Game {
-    
+	private int check;
+	private String result;
+	Random random = new Random();
+	
+	public void setCheck() {
+		check = random.nextInt(3)+1;
+	}
+	public int getCheck() {
+		return check;
+	}
+	public void setResult() {
+		if(check == 1) {
+			result = "Rock";
+		}
+		else if(check == 2) {
+			result = "Paper";
+		}
+		else{
+			result = "Scissor";
+		}
+	}
+	public String getCheck() {
+		return check;
+	}
 }
