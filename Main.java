82cb55a77c058dda777d3298aacac39a190d7c61
sj_mon <122343583+seongju1015@users.noreply.github.com>
2023-08-17 17:11:45 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,23 @@
+package project.game;
+import project.game.Game;
+import project.game.User;
+
 public class Main {
-    
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Main m = new Main();
+		m.run();
+
+	}
+	
+	void run() {
+		Game g = new Game();
+		User u = new User();
+		
+		while(true) {
+			
+		}
+	}
+
 }
