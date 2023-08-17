6b1a342446785628caed2b7888bab56c8ad9b913
sj_mon <122343583+seongju1015@users.noreply.github.com>
2023-08-17 17:21:26 +0900

    Update HelpPage.java

--- HelpPage.java
+++ HelpPage.java
@@ -1,9 +1,15 @@
 package project.game;
 
 public class HelpPage {
-	public void setPage() {
-		System.out.print("-----------------------");
+	public int setPage(int number) {
 		
+		System.out.println("-----------------------");
+		System.out.println("1. show list");
+		System.out.println("2. add user");
+		System.out.println("3. update user");
+		System.out.println("4. delete user");
+		
+		return number;
 	}
 
 }
